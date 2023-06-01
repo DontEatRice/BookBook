3ba2a23d9945f05026cf7ebf2b4c27920274f8ce
jgulinski <gulinskijakub@gmail.com>
2023-06-01 22:04:13 +0200

    Refactor

--- Server/src/Server.Domain/Entities/Publisher.cs
+++ Server/src/Server.Domain/Entities/Publisher.cs
@@ -8,6 +8,7 @@ namespace Server.Domain.Entities;
 public class Publisher
 {
     public Guid Id { get; set; }
-    [Required, MaxLength(50)] public string Name { get; set; }
+    [Required, MaxLength(50)]
+    public string Name { get; set; }
     public ICollection<Book> Books { get; set; }
 }
