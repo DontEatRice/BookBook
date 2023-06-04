e09313a4817f8256890b8a44288e93bdc776c3d3
s22561 <s22561@pjwstk.edu.pl>
2023-06-04 18:18:10 +0200

    Added flow for publisher

---
+++ Server/src/Server.Api/Controllers/PublishersControllers.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Server.Application.Abstractions;
+
+namespace Server.Api.Controllers
+{
+    public class PublishersControllers : ControllerBase
+    {
+        private readonly IQueryHandler<GetPublishers, IEnumerable<PublisherViewModel>> _getPublishersHandler;
+        private readonly IQueryHandler<GetPublisher, PublisherViewModel> _getPublisherHandler;
+
+        private readonly ICommandHandler<AddPublisher, Guid> _addPublisherHandler;
+        private readonly ICommandHandler<RemovePublisher, bool> _removePublisherHandler;
+
+        public PublishersController(
+            IQueryHandler<GetPublishers, IEnumerable<PublisherViewModel>> getPublishersHandler,
+            IQueryHandler<GetPublisher, PublisherViewModel> getPublisherHandler,
+            ICommandHandler<AddPublisher, Guid> addPublisherHandler,
+            ICommandHandler<RemovePublisher, bool> removePublisherHandler)
+        {
+            _getPublishersHandler = getPublishersHandler;
+            _getPublisherHandler = getPublisherHandler;
+            _addPublisherHandler = addPublisherHandler;
+            _removePublisherHandler = removePublisherHandler;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PublisherViewModel>>> List()
+            => Ok(await _getPublishersHandler.HandleAsync(new GetPublishers()));
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<PublisherViewModel>> Get(Guid id)
+            => Ok(await _getPublisherHandler.HandleAsync(new GetPublisher(id)));
+
+        [HttpPost]
+        public async Task<ActionResult> Post(AddPublisher command)
+        {
+            var id = await _addPublisherHandler.HandleAsync(command);
+            return Created($"/Publishers/{id}", null);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult> Post(Guid id)
+        {
+            await _removePublisherHandler.HandleAsync(new RemovePublisher(id));
+            return NoContent();
+        }
+    }
+}
---
+++ Server/src/Server.Domain/Repositories/IPublisherRepository.cs
@@ -0,0 +1,10 @@
+﻿using Server.Domain.Entities;
+
+namespace Server.Domain.Repositories;
+
+public interface IPublisherRepository
+{
+    void Add(Publisher publisher);
+    void Delete(Publisher publisher);
+    Task<Publisher?> FirstOrDefaultByIdAsync(Guid guid);
+}
