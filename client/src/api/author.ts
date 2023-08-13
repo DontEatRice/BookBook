118ee04bafecf4101b77d9003402fe4a97d24ec4
Jakub Sikorski <jakub.sikorski0211@gmail.com>
2023-08-13 18:39:44 +0200

    Pobieranie listy autorow

--- client/src/api/author.ts
+++ client/src/api/author.ts
@@ -1,4 +1,5 @@
 import { AddAuthorType } from '../models/AddAuthor';
+import AuthorViewModel from '../models/AuthorViewModel';
 
 const base = import.meta.env.VITE_API_BASE_URL;
 
@@ -10,3 +11,12 @@ export const postAuthor = async (author: AddAuthorType) => {
   });
   return response;
 };
+
+export async function getAuthors() {
+  const response = await fetch(base + '/Authors');
+  const data = await response.json();
+  //https://zod.dev/?id=basic-usage
+  //można też użyć funkcji .safeParse(data), która nie rzucałaby błędem
+  //w takim przypadku można by coś zlogować i wyświetlić stosowny komunikat
+  return AuthorViewModel.array().parse(data);
+}
--- client/src/pages/admin/AdminAuthors.tsx
+++ client/src/pages/admin/AdminAuthors.tsx
@@ -1,10 +1,48 @@
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
+import { AuthorViewModelType } from '../../models/AuthorViewModel';
+import { useQuery } from 'react-query';
+import { getAuthors } from '../../api/author';
+import { useTheme } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+
+function AuthorsTable({ data }: { data: AuthorViewModelType[] }) {
+  return (
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell>Imię</TableCell>
+            <TableCell>Nazwisko</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((author) => (
+            <TableRow key={author.id}>
+              <TableCell>{author.id}</TableCell>
+              <TableCell>{author.firstName}</TableCell>
+              <TableCell>{author.lastName}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
 
 function AdminAuthors() {
+  const theme = useTheme();
+  const { data, status } = useQuery({ queryKey: ['authors'], queryFn: getAuthors });
+
   return (
     <Box mt={1}>
       <Grid container spacing={2} justifyContent="space-between">
@@ -17,6 +55,13 @@ function AdminAuthors() {
           </Link>
         </Grid>
       </Grid>
+      {status == 'loading' && <Typography variant="h3">Ładowanie...</Typography>}
+      {status == 'error' && (
+        <Typography variant="h3" color={theme.palette.error.main}>
+          Błąd!
+        </Typography>
+      )}
+      {status == 'success' && <AuthorsTable data={data} />}
     </Box>
   );
 }
