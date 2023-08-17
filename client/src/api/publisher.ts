25aca1e0ee59d42fc55d7b07190e1964cec9a92e
s22561 <s22561@pjwstk.edu.pl>
2023-08-17 17:55:14 +0200

    Added publishers fetching

--- client/src/api/publisher.ts
+++ client/src/api/publisher.ts
@@ -1,4 +1,5 @@
 import { AddPublisherType } from '../models/AddPublisher';
+import PublisherViewModel from '../models/PublisherViewModel';
 
 const base = import.meta.env.VITE_API_BASE_URL;
 
@@ -10,3 +11,12 @@ export const postPublisher = async (publisher: AddPublisherType) => {
     });
     return response;
 };
+
+export async function getPublishers() {
+    const response = await fetch(base + '/Publishers');
+    const data = await response.json();
+    //https://zod.dev/?id=basic-usage
+    //można też użyć funkcji .safeParse(data), która nie rzucałaby błędem
+    //w takim przypadku można by coś zlogować i wyświetlić stosowny komunikat
+    return PublisherViewModel.array().parse(data);
+  }
