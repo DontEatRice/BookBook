77e2c0309c69813831d79005add8fe6cb7f19611
Jakub Sikorski <jakub.sikorski0211@gmail.com>
2023-08-19 18:19:21 +0200

    dodawanie zdjecia

--- client/src/models/AddAuthor.ts
+++ client/src/models/AddAuthor.ts
@@ -1,6 +1,23 @@
 import { z } from 'zod';
 
+const ACCEPTED_FORMATS = ['image/png', 'image/jpg', 'image/jpeg'];
+
 const AddAuthor = z.object({
+  avatarPicture: z
+    .custom<FileList>((v) => v == undefined || v instanceof FileList)
+    .optional()
+    .transform((val) => val?.item(0))
+    .refine((file) => {
+      console.log(file);
+      return true;
+    })
+    .refine((file) => file == undefined || file.size <= 1_000_000, {
+      message: 'Plik musi być mniejszy niż 1 MB.',
+    })
+    .refine(
+      (file) => file == undefined || ACCEPTED_FORMATS.includes(file.type),
+      'Akceptowane pliki to .png i .jpg'
+    ),
   firstName: z.string().max(40).nonempty(),
   lastName: z.string().max(50).nonempty(),
 });
--- client/src/pages/admin/AdminAuthorForm.tsx
+++ client/src/pages/admin/AdminAuthorForm.tsx
@@ -1,5 +1,5 @@
 import Box from '@mui/material/Box';
-import { useForm } from 'react-hook-form';
+import { useForm, useWatch } from 'react-hook-form';
 import AddAuthor, { AddAuthorType } from '../../models/AddAuthor';
 import { zodResolver } from '@hookform/resolvers/zod';
 import Paper from '@mui/material/Paper';
@@ -8,20 +8,32 @@ import TextInputField from '../../components/TextInputField';
 import { useMutation } from 'react-query';
 import { postAuthor } from '../../api/author';
 import { useNavigate } from 'react-router-dom';
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 
 function AdminAuthorForm() {
   const navigate = useNavigate();
   const {
     register,
     handleSubmit,
+    control,
     formState: { errors },
   } = useForm<AddAuthorType>({
     resolver: zodResolver(AddAuthor),
   });
+  const watchAvatarPicture = useWatch({
+    control,
+    name: 'avatarPicture',
+  });
+  const fileName = useMemo(() => {
+    if (watchAvatarPicture instanceof FileList) {
+      return watchAvatarPicture.item(0)?.name;
+    }
+    return undefined;
+  }, [watchAvatarPicture]);
   const mutation = useMutation({
     mutationFn: postAuthor,
     onSuccess: () => {
+      // response.headers.has() TODO dodać redirect na nowy obiekt
       navigate('..');
     },
     onError: (e: Error) => {
@@ -31,15 +43,34 @@ function AdminAuthorForm() {
   const onSubmit = useCallback(
     (data: AddAuthorType) => {
       mutation.mutate(data);
+      // console.log(data);
     },
     [mutation]
   );
-
   return (
     <Box sx={{ mt: 2 }}>
       <form style={{ display: 'flex', justifyContent: 'center' }} onSubmit={handleSubmit(onSubmit)}>
         <Box sx={{ width: { xs: '100%', sm: '85%', md: '65%' }, textAlign: 'center' }}>
           <Paper sx={{ p: 2, width: '100%' }} elevation={3}>
+            <Box
+              component="label"
+              htmlFor="upload-photo"
+              sx={{ width: '100%', display: 'inline-flex', mb: 2 }}>
+              <input
+                style={{ display: 'none' }}
+                id="upload-photo"
+                // name="upload-photo"
+                type="file"
+                accept="image/png,image/jpg,image/jpeg"
+                {...register('avatarPicture')}
+              />
+
+              <Button variant="contained" component="span">
+                Wstaw zdjęcie (opcjonalnie)
+              </Button>
+              {fileName && <span>{fileName}</span>}
+            </Box>
+            {errors.avatarPicture != undefined && <span>{errors.avatarPicture.message}</span>}
             <TextInputField errors={errors} field="firstName" label="Imię" register={register} />
             <TextInputField errors={errors} field="lastName" label="Nazwisko/Nazwiska" register={register} />
             <Button type="submit" variant="contained">
