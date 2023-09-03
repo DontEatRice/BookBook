97b84be627d55b5a98d1d84158689783d8ad9dc5
Jakub Sikorski <jakub.sikorski0211@gmail.com>
2023-09-03 22:17:36 +0200

    fix importow

--- client/src/pages/admin/AdminLibraryForm.tsx
+++ client/src/pages/admin/AdminLibraryForm.tsx
@@ -6,7 +6,6 @@ import Paper from '@mui/material/Paper';
 import { useMutation } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 import TextInputField from '../../components/TextInputField';
-import { Accordion, AccordionDetails, Divider, Stack, Typography } from '@mui/material';
 import NumberInputField from '../../components/NumberInputField';
 import AddLibrary, { AddLibraryType } from '../../models/AddLibrary';
 import { postLibrary } from '../../api/library';
@@ -15,97 +14,118 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import TimeInputField from '../../components/TimeInputField';
-import { TimePicker } from '@material-ui/pickers'
+import Typography from '@mui/material/Typography';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Stack from '@mui/material/Stack';
+import Divider from '@mui/material/Divider';
 
 function AdminLibraryForm() {
-    const navigate = useNavigate();
-    const {
-        register,
-        handleSubmit,
-        formState: { errors }
-    } = useForm<AddLibraryType>({
-        resolver: zodResolver(AddLibrary),
-    });
-    const mutation = useMutation({
-        mutationFn: postLibrary,
-        onSuccess: () => {
-            navigate('..');
-        },
-        onError: (e: Error) => {
-            console.error(e);
-        },
-    });
-    const onSubmit: SubmitHandler<AddLibraryType> = (data) => {
-        console.log(data);
-        mutation.mutate(data);
-    };
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<AddLibraryType>({
+    resolver: zodResolver(AddLibrary),
+  });
+  const mutation = useMutation({
+    mutationFn: postLibrary,
+    onSuccess: () => {
+      navigate('..');
+    },
+    onError: (e: Error) => {
+      console.error(e);
+    },
+  });
+  const onSubmit: SubmitHandler<AddLibraryType> = (data) => {
+    console.log(data);
+    mutation.mutate(data);
+  };
 
-    return (
-        <Box sx={{ mt: 2 }}>
-            <form onSubmit={handleSubmit(onSubmit)} style={{ display: 'flex', justifyContent: 'center' }}>
-                <Box
-                    sx={{
-                        width: { xs: '100%', sm: '85%', md: '65%' },
-                        textAlign: 'center',
-                    }}>
-                    <Paper sx={{ p: 2, width: '100%' }} elevation={3}>
-                        <Accordion sx={{ width: '100%', mb: 2 }}>
-                            <AccordionSummary
-                                expandIcon={<ExpandMoreIcon />}
-                                aria-controls="panel1a-content"
-                                id="panel1a-header"
-                            >
-                                <Typography>Informacje ogólne</Typography>
-                            </AccordionSummary>
-                            <AccordionDetails sx={{ width: '100%', mb: 2 }}>
-                                <TextInputField errors={errors} field="name" register={register} label="Nazwa" />
-                                <NumberInputField errors={errors} field="hireTime" register={register} label="Czas wypożyczenia" />
-                                <NumberInputField errors={errors} field="reservationTime" register={register} label="Czas rezerwacji" />
-                            </AccordionDetails>
-                        </Accordion>
-                        <Accordion sx={{ width: '100%', mb: 2 }}>
-                            <AccordionSummary
-                                expandIcon={<ExpandMoreIcon />}
-                                aria-controls="panel1a-content"
-                                id="panel1a-header"
-                            >
-                                <Typography>Adres</Typography>
-                            </AccordionSummary>
-                            <AccordionDetails sx={{ width: '100%', mb: 2 }}>
-                                <TextInputField errors={errors} field="street" register={register} label="Ulica" />
-                                <TextInputField errors={errors} field="number" register={register} label="Numer" />
-                                <TextInputField errors={errors} field="apartment" register={register} label="Numer lokalu" />
-                                <TextInputField errors={errors} field='city' register={register} label="Miasto" />
-                                <TextInputField errors={errors} field="postalCode" register={register} label="Kod pocztowy" />
-                                <TextInputField errors={errors} field="additionalInfo" register={register} label="Informacje dodatkowe" />
-                            </AccordionDetails>
-                        </Accordion>
-                        <Accordion sx={{ width: '100%', mb: 2 }}>
-                            <AccordionSummary
-                                expandIcon={<ExpandMoreIcon />}
-                                aria-controls="panel1a-content"
-                                id="panel1a-header"
-                            >
-                                <Typography>Godziny otwarcia</Typography>
-                            </AccordionSummary>
-                            <AccordionDetails sx={{ width: '100%', mb: 2 }}>
-                                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                    <Typography gutterBottom>Poniedziałek</Typography>
-                                    <Stack alignItems={'center'} sx={{ width: '100%', mb: 2 }} direction="row" spacing={2} divider={<Divider orientation="vertical" flexItem />}>
-                                        <TimeInputField
-                                            label="Otwarcie"
-                                            register={register}
-                                            field='mondayOpenTime'
-                                            errors={errors}
-                                        />
-                                        <TimeInputField
-                                            label="Zamknięcie"
-                                            register={register}
-                                            field='mondayCloseTime'
-                                            errors={errors}
-                                        />
-                                    </Stack>
-                                    {/* <Typography gutterBottom>Wtorek</Typography>
+  return (
+    <Box sx={{ mt: 2 }}>
+      <form onSubmit={handleSubmit(onSubmit)} style={{ display: 'flex', justifyContent: 'center' }}>
+        <Box
+          sx={{
+            width: { xs: '100%', sm: '85%', md: '65%' },
+            textAlign: 'center',
+          }}>
+          <Paper sx={{ p: 2, width: '100%' }} elevation={3}>
+            <Accordion sx={{ width: '100%', mb: 2 }}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header">
+                <Typography>Informacje ogólne</Typography>
+              </AccordionSummary>
+              <AccordionDetails sx={{ width: '100%', mb: 2 }}>
+                <TextInputField errors={errors} field="name" register={register} label="Nazwa" />
+                <NumberInputField
+                  errors={errors}
+                  field="hireTime"
+                  register={register}
+                  label="Czas wypożyczenia"
+                />
+                <NumberInputField
+                  errors={errors}
+                  field="reservationTime"
+                  register={register}
+                  label="Czas rezerwacji"
+                />
+              </AccordionDetails>
+            </Accordion>
+            <Accordion sx={{ width: '100%', mb: 2 }}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header">
+                <Typography>Adres</Typography>
+              </AccordionSummary>
+              <AccordionDetails sx={{ width: '100%', mb: 2 }}>
+                <TextInputField errors={errors} field="street" register={register} label="Ulica" />
+                <TextInputField errors={errors} field="number" register={register} label="Numer" />
+                <TextInputField errors={errors} field="apartment" register={register} label="Numer lokalu" />
+                <TextInputField errors={errors} field="city" register={register} label="Miasto" />
+                <TextInputField errors={errors} field="postalCode" register={register} label="Kod pocztowy" />
+                <TextInputField
+                  errors={errors}
+                  field="additionalInfo"
+                  register={register}
+                  label="Informacje dodatkowe"
+                />
+              </AccordionDetails>
+            </Accordion>
+            <Accordion sx={{ width: '100%', mb: 2 }}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header">
+                <Typography>Godziny otwarcia</Typography>
+              </AccordionSummary>
+              <AccordionDetails sx={{ width: '100%', mb: 2 }}>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <Typography gutterBottom>Poniedziałek</Typography>
+                  <Stack
+                    alignItems={'center'}
+                    sx={{ width: '100%', mb: 2 }}
+                    direction="row"
+                    spacing={2}
+                    divider={<Divider orientation="vertical" flexItem />}>
+                    <TimeInputField
+                      label="Otwarcie"
+                      register={register}
+                      field="mondayOpenTime"
+                      errors={errors}
+                    />
+                    <TimeInputField
+                      label="Zamknięcie"
+                      register={register}
+                      field="mondayCloseTime"
+                      errors={errors}
+                    />
+                  </Stack>
+                  {/* <Typography gutterBottom>Wtorek</Typography>
                                     <Stack alignItems={'center'} sx={{ width: '100%', mb: 2 }} direction="row" spacing={2} divider={<Divider orientation="vertical" flexItem />}>
                                         <TimeInputField
                                             label="Otwarcie"
@@ -195,18 +215,17 @@ function AdminLibraryForm() {
                                             errors={errors}
                                         />
                                     </Stack> */}
-                                </LocalizationProvider>
-                            </AccordionDetails>
-                        </Accordion>
-                        <Button type="submit" variant="contained">
-                            Zapisz
-                        </Button>
-                    </Paper>
-                </Box>
-            </form>
+                </LocalizationProvider>
+              </AccordionDetails>
+            </Accordion>
+            <Button type="submit" variant="contained">
+              Zapisz
+            </Button>
+          </Paper>
         </Box>
-    );
-
+      </form>
+    </Box>
+  );
 }
 
 export default AdminLibraryForm;
