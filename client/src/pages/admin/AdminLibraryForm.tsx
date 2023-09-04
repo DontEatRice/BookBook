143aca17ce183bd99e41d06b507e3acd90082f17
Jakub Sikorski <jakub.sikorski0211@gmail.com>
2023-09-04 18:49:52 +0200

    wydzielenie tcyh dni tygodnia do innego komponentu

--- client/src/pages/admin/AdminLibraryForm.tsx
+++ client/src/pages/admin/AdminLibraryForm.tsx
@@ -1,4 +1,4 @@
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { FieldErrors, SubmitHandler, UseFormRegister, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
@@ -11,14 +11,41 @@ import AddLibrary, { AddLibraryType } from '../../models/AddLibrary';
 import { postLibrary } from '../../api/library';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import TimeInputField from '../../components/TimeInputField';
 import Typography from '@mui/material/Typography';
 import Accordion from '@mui/material/Accordion';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import { Checkbox } from '@mui/material';
+
+type LibraryOpenHoursTimePickerParams = {
+  register: UseFormRegister<AddLibraryType>;
+  errors: FieldErrors<AddLibraryType>;
+  fields: [keyof AddLibraryType, keyof AddLibraryType];
+  dayName: string;
+};
+
+function LibraryOpenHoursTimePicker({ register, errors, fields, dayName }: LibraryOpenHoursTimePickerParams) {
+  return (
+    <Box>
+      <Typography variant="h5" textAlign={'left'}>
+        {dayName}
+      </Typography>
+      <Stack
+        alignItems={'center'}
+        sx={{ width: '100%', mb: 2 }}
+        direction="row"
+        spacing={2}
+        divider={<Divider orientation="vertical" flexItem />}>
+        <FormControlLabel label="Otwarte?" control={<Checkbox defaultChecked />} />
+        <TimeInputField label="Otwarcie" register={register} field={fields[0]} errors={errors} />
+        <TimeInputField label="Zamknięcie" register={register} field={fields[1]} errors={errors} />
+      </Stack>
+    </Box>
+  );
+}
 
 function AdminLibraryForm() {
   const navigate = useNavigate();
@@ -48,15 +75,15 @@ function AdminLibraryForm() {
       <form onSubmit={handleSubmit(onSubmit)} style={{ display: 'flex', justifyContent: 'center' }}>
         <Box
           sx={{
-            width: { xs: '100%', sm: '85%', md: '65%' },
+            width: { xs: '100%', sm: '90%', md: '80%' },
             textAlign: 'center',
           }}>
           <Paper sx={{ p: 2, width: '100%' }} elevation={3}>
             <Accordion sx={{ width: '100%', mb: 2 }}>
               <AccordionSummary
                 expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header">
+                aria-controls="generalInfo-content"
+                id="generalInfo-header">
                 <Typography>Informacje ogólne</Typography>
               </AccordionSummary>
               <AccordionDetails sx={{ width: '100%', mb: 2 }}>
@@ -104,28 +131,34 @@ function AdminLibraryForm() {
                 <Typography>Godziny otwarcia</Typography>
               </AccordionSummary>
               <AccordionDetails sx={{ width: '100%', mb: 2 }}>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <Typography gutterBottom>Poniedziałek</Typography>
-                  <Stack
-                    alignItems={'center'}
-                    sx={{ width: '100%', mb: 2 }}
-                    direction="row"
-                    spacing={2}
-                    divider={<Divider orientation="vertical" flexItem />}>
-                    <TimeInputField
-                      label="Otwarcie"
-                      register={register}
-                      field="mondayOpenTime"
-                      errors={errors}
-                    />
-                    <TimeInputField
-                      label="Zamknięcie"
-                      register={register}
-                      field="mondayCloseTime"
-                      errors={errors}
-                    />
-                  </Stack>
-                  {/* <Typography gutterBottom>Wtorek</Typography>
+                {/* <Typography gutterBottom>Poniedziałek</Typography> */}
+                <Stack
+                  alignItems={'center'}
+                  sx={{ width: '100%', mb: 2 }}
+                  direction="row"
+                  spacing={2}
+                  divider={<Divider orientation="vertical" flexItem />}>
+                  <FormControlLabel label="Otwarte?" control={<Checkbox defaultChecked />} />
+                  <TimeInputField
+                    label="Otwarcie"
+                    register={register}
+                    field="mondayOpenTime"
+                    errors={errors}
+                  />
+                  <TimeInputField
+                    label="Zamknięcie"
+                    register={register}
+                    field="mondayCloseTime"
+                    errors={errors}
+                  />
+                </Stack>
+                <LibraryOpenHoursTimePicker
+                  dayName="Poniedziałek"
+                  errors={errors}
+                  fields={['mondayOpenTime', 'mondayCloseTime']}
+                  register={register}
+                />
+                {/* <Typography gutterBottom>Wtorek</Typography>
                                     <Stack alignItems={'center'} sx={{ width: '100%', mb: 2 }} direction="row" spacing={2} divider={<Divider orientation="vertical" flexItem />}>
                                         <TimeInputField
                                             label="Otwarcie"
@@ -215,10 +248,9 @@ function AdminLibraryForm() {
                                             errors={errors}
                                         />
                                     </Stack> */}
-                </LocalizationProvider>
               </AccordionDetails>
             </Accordion>
-            <Button type="submit" variant="contained">
+            <Button type="submit" variant="contained" sx={{ mt: 2 }}>
               Zapisz
             </Button>
           </Paper>
