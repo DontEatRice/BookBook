1632ac3473cffd2aedce04c5f511b01d4bd1afc3
Michał Januszewski <michaljn2@gmail.com>
2023-09-12 21:52:37 +0200

    Move Admin header above other components

--- client/src/shared/AdminHeader.tsx
+++ client/src/shared/AdminHeader.tsx
@@ -15,6 +15,7 @@ function AdminHeader() {
         color: theme.palette.text.primary,
         backgroundColor: theme.palette.grey[100],
         padding: theme.spacing(2),
+        zIndex: 3
       }}>
       <Link to="/admin" style={{ textDecoration: 'none' }}>
         <Typography
