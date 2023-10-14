8c9ba4c7efbf6028445571a3f66b9c009849856f
Jakub Sikorski <jakub.sikorski0211@gmail.com>
2023-10-14 15:37:07 +0200

    migracja ze zdjęciami

---
+++ Server/src/Server.Infrastructure/Persistence/Migrations/20231014133622_AddImages.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePictureUrl",
+                table: "Authors",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ContentType = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    Content = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    FileName = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    Etag = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "ProfilePictureUrl",
+                table: "Authors");
+        }
+    }
+}
