8d4b02c54323de8eae808629960127e487183353
Jakub Guliński <gulinskijakub@gmail.com>
2023-10-21 19:54:07 +0200

    Add missing files

---
+++ client/src/pages/book/BooksList.tsx
@@ -0,0 +1,72 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+import { BookViewModelType } from '../../models/BookViewModel';
+import { useTheme } from '@mui/material/styles';
+import { searchBooks } from '../../api/book';
+import { Grid } from '@mui/material';
+import BookInList from '../../components/BookInList';
+import { useQuery } from '@tanstack/react-query';
+import { useLocation } from 'react-router-dom';
+
+// przyklad z https://mui.com/material-ui/react-table/#sorting-amp-selecting
+
+// function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+//   if (b[orderBy] < a[orderBy]) {
+//     return -1;
+//   }
+//   if (b[orderBy] > a[orderBy]) {
+//     return 1;
+//   }
+//   return 0;
+// }
+
+// type Order = 'asc' | 'desc';
+
+// function getComparator<Key extends keyof BookViewModelType>(
+//   order: Order,
+//   orderBy: Key
+// ): (a: BookViewModelType, b: BookViewModelType) => number {
+//   return order === 'desc'
+//     ? (a, b) => descendingComparator(a, b, orderBy)
+//     : (a, b) => -descendingComparator(a, b, orderBy);
+// }
+
+function Books({ data }: { data: BookViewModelType[] }) {
+  return (
+    <Grid container spacing={1}>
+      {data.map((book) => (
+        <Grid key={book.id} item xs={6}>
+          <BookInList book={book} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
+
+function BooksList() {
+  const theme = useTheme();
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const q = queryParams.get('q');
+  //const { data, status } = useQuery({ queryKey: ['books'], queryFn: getBooks });
+  const { data: searchData, status: searchStatus } = useQuery({
+    queryKey: ['searchBooks', q],
+    queryFn: () => searchBooks(q == null ? '' : q),
+  });
+
+  return (
+    <Box>
+      {searchStatus == 'loading' && <Typography variant="h3">Ładowanie...</Typography>}
+      {searchStatus == 'error' && (
+        <Typography variant="h3" color={theme.palette.error.main}>
+          Błąd!
+        </Typography>
+      )}
+      {searchStatus == 'success' && <Books data={searchData} />}
+    </Box>
+  );
+}
+
+export default BooksList;
+
