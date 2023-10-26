39d4ef307f6c48332607dc5fb9bdc6f5dab69e48
bmikolajj <barcik.mikolaj1@gmail.com>
2023-10-26 18:26:09 +0200

    Added pop-up for editing review in frontend

--- client/src/pages/book/BookDetails.tsx
+++ client/src/pages/book/BookDetails.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
-import { Typography, TableHead, TableBody, Table, Rating, Grid, Box, TableRow, TableContainer, Paper, Avatar, Button, Hidden } from '@mui/material';
+import {useState} from 'react'
+import { Dialog, TextField, Typography, TableHead, TableBody, Table, Rating, Grid, Box, TableRow, TableContainer, Paper, Avatar, Button, Hidden, FilledInput } from '@mui/material';
 import FilledField from '../../components/FilledField';
 import StyledTableCell from '../../components/tableComponents/StyledTableCell';
 import StyledTableRow from '../../components/tableComponents/StyledTableRow';
 import TextInputField from '../../components/TextInputField';
+import TextEditField from '../../components/TextEditField';
 import { useParams } from 'react-router';
 import { AuthorViewModelType } from '../../models/AuthorViewModel';
 import { getBook } from '../../api/book';
@@ -20,6 +22,7 @@ import { useMutation } from '@tanstack/react-query';
 import { deleteReview, postReview } from '../../api/review';
 import AddReview, { AddReviewType } from '../../models/AddReview';
 import { BookViewModelType } from '../../models/BookViewModel';
+import { fileToBase64 } from '../../utils/utils';
 
 function AuthorsTable({ authors }: { authors: AuthorViewModelType[] }) {
   const authorNames = authors.map((author) => `${author.firstName} ${author.lastName}`).join(', ');
@@ -34,18 +37,6 @@ function CategoryTable({ categories }: { categories: BookCategoryViewModelType[]
 }
 
 function ReviewsTable({ reviews, book }: { reviews: ReviewViewModelType[], book: BookViewModelType }) {
-  const theme = useTheme();
-
-  const mutation = useMutation({
-    mutationFn: deleteReview,
-    onSuccess: () => {
-      window.location.reload();
-    },
-    onError: (e: Error) => {
-      console.error(e);
-    },
-  });
-
   return (
     <TableContainer component={Paper} sx={{ maxWidth: 800}}>
     <Table aria-label="customized table">
@@ -59,7 +50,40 @@ function ReviewsTable({ reviews, book }: { reviews: ReviewViewModelType[], book:
       </TableHead>
       <TableBody>
       {reviews.map((review) => (
-          <StyledTableRow key={review.id}>
+          <ReviewTableRow review={review} book={book}></ReviewTableRow>
+        ))}
+      </TableBody>
+    </Table>
+    <Grid item><ReviewForm book={book}/></Grid>
+  </TableContainer>
+  );
+}
+
+function ReviewTableRow({ review, book }: { review: ReviewViewModelType, book: BookViewModelType}) {
+  const theme = useTheme();
+
+  const [open, setOpen] = useState(false)
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  const mutation = useMutation({
+    mutationFn: deleteReview,
+    onSuccess: () => {
+      window.location.reload();
+    },
+    onError: (e: Error) => {
+      console.error(e);
+    },
+  });
+
+  return (
+    <StyledTableRow key={review.id}>
             <StyledTableCell >
               <Avatar sx={{ bgcolor: theme.palette.secondary.main }}>N</Avatar>
               <div>username</div>
@@ -67,26 +91,93 @@ function ReviewsTable({ reviews, book }: { reviews: ReviewViewModelType[], book:
             <StyledTableCell>
             <Rating
               name="half-rating-read"
-              value={review.rating == null ? 0 : review.rating}
-              precision={0.25}
+              defaultValue={review.rating == null ? 0 : review.rating}
+              precision={1}
               readOnly
             />
             </StyledTableCell>
             <StyledTableCell>
-            <Typography variant="h5">{review.title}</Typography>
-              {review.description}
+              <Typography variant="h5">
+                <TextField multiline maxRows={2} InputProps={{readOnly: true}} sx={{ mb: 2 }} variant="standard" defaultValue={review.title} />
+              </Typography>
+                <TextField multiline InputProps={{readOnly: true}} sx={{ mb: 2 }} defaultValue={review.description} />
             </StyledTableCell>
             <StyledTableCell>
               <Button sx={{ width: 50, height: 40 }} onClick={() => mutation.mutate(review.id)}>
                 Usuń
               </Button>
+              <Button sx={{ width: 50, height: 40 }} onClick={handleOpen}>
+                Edytuj
+              </Button>
+              <ModalDialog open={open} handleClose={handleClose} book={book} review={review}/>
             </StyledTableCell>
-          </StyledTableRow>
-        ))}
-      </TableBody>
-    </Table>
-    <Grid item><ReviewForm book={book}/></Grid>
-  </TableContainer>
+      </StyledTableRow>
+  )
+}
+
+const ModalDialog = (props: {open: boolean, review: ReviewViewModelType, book: BookViewModelType, handleClose: () => void}) => {
+  return (
+      <Dialog open={props.open} onClose={props.handleClose}>
+          <EditReviewForm review={props.review} book={props.book} handleClose={props.handleClose}/>
+      </Dialog>
+      )
+}
+
+function EditReviewForm({ review, book, handleClose }: { review: ReviewViewModelType, book: BookViewModelType, handleClose: () => void }) {
+  const [value, setValue] = React.useState<number | null>(review.rating);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<AddReviewType>({
+    resolver: zodResolver(AddReview),
+  });
+  const mutation = useMutation({
+    mutationFn: postReview,
+    onSuccess: () => {
+      handleClose();
+      window.location.reload();
+    },
+    onError: (e: Error) => {
+      console.error(e);
+    },
+  });
+  const onSubmit: SubmitHandler<AddReviewType> = (data) => {
+    data.rating = value == null ? 0 : value;
+    data.idBook = book.id;
+    mutation.mutate(data);
+  };
+  
+  return (
+    <Box sx={{ mt: 2 }}>
+      <form onSubmit={handleSubmit(onSubmit)} style={{ display: 'flex', justifyContent: 'center' }}>
+        <Box
+          sx={{
+            width: { xs: '100%', sm: '85%', md: '65%' },
+            textAlign: 'left',
+          }}>
+          <Paper sx={{ p: 2, width: '100%' }} elevation={3}>
+            <Rating
+              name="simple-controlled"
+              value={value}
+              onChange={(event, newValue) => {
+                setValue(newValue);
+              }}
+            />
+            <TextEditField errors={errors} field="title" register={register} label="Tytuł" defaultValue={review.title+""}/>
+            <TextEditField errors={errors} field="description" register={register} label="Komentarz" defaultValue={review.description+""}/>
+            <input type="hidden" {...register("idBook")} value={book.id}/>
+            <Button type="submit" variant="contained">
+              Uaktualnij
+            </Button>
+            <Button variant="contained" onClick={() => handleClose()}>
+              Anuluj
+            </Button>
+          </Paper>
+        </Box>
+      </form>
+    </Box>
   );
 }
 
