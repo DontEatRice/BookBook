12d67c739b95ed460a6a7010a1bf3b046588bafc
Michał Januszewski <michaljn2@gmail.com>
2023-12-07 23:55:02 +0100

    Wiadomosc o braku obserwowanych książek

--- client/src/pages/user/UserBooks.tsx
+++ client/src/pages/user/UserBooks.tsx
@@ -5,17 +5,31 @@ import BookInUserList from '../../components/book/BookInUserList';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LoadingTypography from '../../components/common/LoadingTypography';
+import { Button, Typography } from '@mui/material';
 
 function UserBooks({ data }: { data: BookViewModelType[] }) {
-  return (
-    <Grid container spacing={3}>
-      {data.map((book) => (
-        <Grid item xs={12} key={book.id}>
-          <BookInUserList book={book} />
-        </Grid>
-      ))}
-    </Grid>
-  );
+  if (data.length == 0) {
+    return (
+      <Box alignContent={'center'} textAlign={'center'}>
+        <Typography variant="h5" sx={{ marginBottom: 3 }}>
+          Nie masz żadnych obserwowanych książek!
+        </Typography>
+        <Button href="/books" variant="contained">
+          Przeglądaj książki
+        </Button>
+      </Box>
+    );
+  } else {
+    return (
+      <Grid container spacing={3}>
+        {data.map((book) => (
+          <Grid item xs={12} key={book.id}>
+            <BookInUserList book={book} />
+          </Grid>
+        ))}
+      </Grid>
+    );
+  }
 }
 
 function UserBooksList() {
