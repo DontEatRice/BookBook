d85a40083788856a9c4adf35995bbc1ab0a63167
Jakub Sikorski <jakub.sikorski0211@gmail.com>
2023-12-14 22:45:11 +0100

    logout endpoint + fe fixes

---
+++ Server/src/Server.Application/CommandHandlers/Auth/LogoutHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Server.Domain.Repositories;
+
+namespace Server.Application.CommandHandlers.Auth;
+
+public sealed record RemoveRefreshTokenCommand(string RefreshToken) : IRequest;
+
+public class RemoveRefreshTokenHandler : IRequestHandler<RemoveRefreshTokenCommand>
+{
+    private readonly IIdentityRepository _identityRepository;
+    private readonly ISecurityTokenService _securityTokenService;
+
+
+    public RemoveRefreshTokenHandler(IIdentityRepository identityRepository)
+    {
+        _identityRepository = identityRepository;
+    }
+
+    public Task Handle(RemoveRefreshTokenCommand request, CancellationToken cancellationToken)
+    {
+        
+    }
+}
