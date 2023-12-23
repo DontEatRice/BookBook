8aecace43af235a771abd22ed868cb501d28c043
Michal <michaljn2@gmail.com>
2023-12-24 00:35:07 +0100

    Fix previous commit

--- Server/src/Server.Infrastructure/Persistence/QueryHandlers/GetLibrariesHandler.cs
+++ Server/src/Server.Infrastructure/Persistence/QueryHandlers/GetLibrariesHandler.cs
@@ -26,9 +26,16 @@ internal sealed class GetLibrariesHandler
     {
         var query = _dbContext.Libraries.AsNoTracking();
 
-        query = !string.IsNullOrWhiteSpace(request.OrderByField)
-            ? query.OrderBy(request.OrderByField)
-            : query.OrderBy(x => x.Id);
+        if (!string.IsNullOrWhiteSpace(request.OrderByField))
+        {
+            query = request.OrderDirection == OrderDirection.Asc
+                ? query.OrderBy(request.OrderByField).ThenBy(x => x.Id)
+                : query.OrderByDescending(request.OrderByField).ThenBy(x => x.Id);
+        }
+        else
+        {
+            query = query.OrderBy(x => x.Id);
+        }
 
         var (libraries, totalCount) = await query
             .Include(x => x.Address)
