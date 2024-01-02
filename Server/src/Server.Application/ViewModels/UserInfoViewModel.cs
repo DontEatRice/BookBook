89d0175fe85e59016ebe58ed8fa0e8d2bcf76e94
Jakub Sikorski <jakub.sikorski0211@gmail.com>
2024-01-02 17:53:21 +0100

    finish

---
+++ Server/src/Server.Application/ViewModels/UserInfoViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Server.Application.ViewModels;
+
+public class UserInfoViewModel
+{
+    
+}
---
+++ Server/src/Server.Infrastructure/Persistence/QueryHandlers/User/GetUserFollowsHandler.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using MediatR;
+using Server.Application.ViewModels;
+using Server.Utils;
+
+namespace Server.Infrastructure.Persistence.QueryHandlers.User;
+
+public sealed record GetUserFollowersQuery : PaginationOptions,
+    IRequest<PaginatedResponseViewModel<UserInfoViewModel>>
+{
+    public Guid UserId { get; set; }
+}
+
+internal sealed class GetUserFollowersHandler : IRequestHandler<GetUserFollowersQuery, PaginatedResponseViewModel<UserInfoViewModel>>
+{
+    private readonly BookBookDbContext _bookDbContext;
+    private readonly IMapper _mapper;
+
+    public GetUserFollowersHandler(BookBookDbContext bookDbContext, IMapper mapper)
+    {
+        _bookDbContext = bookDbContext;
+        _mapper = mapper;
+    }
+
+    public async Task<PaginatedResponseViewModel<UserInfoViewModel>> Handle(GetUserFollowersQuery request, CancellationToken cancellationToken)
+    {
+        var (result, count) = await _bookDbContext.Follows
+            .OrderBy(x => x.FollowerId)
+            .Where(x => x.FollowedId == request.UserId)
+            .Select(x => new UserInfoViewModel
+            {
+                UserName = x.Follower.Name ?? "",
+                IsCritic = x.Follower.IsCritic,
+                UserImageUrl = x.Follower.AvatarImageUrl,
+                Id = x.FollowerId,
+                AboutMe = x.Follower.AboutMe
+            })
+            .ToListWithOffsetAsync(request.PageNumber, request.PageSize, cancellationToken);
+        
+        return PaginatedResponseViewModel.Create(result, count, request);
+    }
+}
