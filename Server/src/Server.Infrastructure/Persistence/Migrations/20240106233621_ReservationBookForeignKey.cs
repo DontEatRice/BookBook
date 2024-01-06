612bdcb62e6c5dc466cb7d3192abbf18f3ed8d11
Michał Januszewski <michaljn2@gmail.com>
2024-01-07 00:38:49 +0100

    Add foreign key to ReservationBook

---
+++ Server/src/Server.Infrastructure/Persistence/Migrations/20240106233621_ReservationBookForeignKey.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReservationBookForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ReservationBook_BookId",
+                table: "ReservationBook",
+                column: "BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ReservationBook_Books_BookId",
+                table: "ReservationBook",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ReservationBook_Books_BookId",
+                table: "ReservationBook");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ReservationBook_BookId",
+                table: "ReservationBook");
+        }
+    }
+}
